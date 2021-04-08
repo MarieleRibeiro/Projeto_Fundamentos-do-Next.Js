44d9301f79d72c32fbb35b4826375be38ceec281
Mariele Ribeiro <mariele_rbr@hotmail.com>
2021-04-08 12:18:43 -0300

    Estratégias de autenticação

---
+++ src/pages/api/users.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+// ESTRATÉGIAS DE AUTENTICAÇÃO
+// -> JWT(storange)- geralmente tem uma data de expiração, coloca essa expiração ate baixa pra poder
+//trabalhar com refhesh token, atualizando eles aos poucos quando o usuario precisa acessar a aplicação de novo
+// -> Next Auth(Social) quando a gente quer um sistema de autenticação simples, e quando precisa de login social
+//ou seja fazer um login de terceiro(com github, com facebook, ...), e consegue utilizar o Next Auth quando não quer
+// se preocupar em ficar armazenando credenciais de acesso do usuario dento do nosso back-end.
+// -> Cognito(AWS) utiliza-se de serviços externos, provider de autenticação externa ou seja esses provider vão
+//se conectar e armazenar dados do usuario, senha, fazer envio de email
