e33b2ed4ce5d1b665f16976bd31b21b7b01d059d
Mariele Ribeiro <mariele_rbr@hotmail.com>
2021-04-20 12:27:18 -0300

    pagina posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -9,15 +9,7 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage
-              multiple packages with a shered build, test and release process.
-            </p>
-          </a>
-          <a>
+          <a href="#">
             <time>12 de março de 2021</time>
             <strong>Creating a Monorepo with, Yarn Workspaces</strong>
             <p>
@@ -25,7 +17,7 @@ export default function Posts() {
               multiple packages with a shered build, test and release process.
             </p>
           </a>
-          <a>
+          <a href="#">
             <time>12 de março de 2021</time>
             <strong>Creating a Monorepo with, Yarn Workspaces</strong>
             <p>
@@ -33,7 +25,15 @@ export default function Posts() {
               multiple packages with a shered build, test and release process.
             </p>
           </a>
-          <a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with, Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shered build, test and release process.
+            </p>
+          </a>
+          <a href="#">
             <time>12 de março de 2021</time>
             <strong>Creating a Monorepo with, Yarn Workspaces</strong>
             <p>
